7e906485ec9e0b70b77fd290c09717783411e113
shivtacty <shivtacty1@gmail.com>
2025-01-18 16:16:09 +0530

    Updates

---
+++ assets/CCardText-Dy8fizNL.js
@@ -0,0 +1,1 @@
+import{r as m,_ as p,R as o,b as i,c as N,P as s}from"./index-DKnGlJMs.js";var r=m.forwardRef(function(e,t){var c=e.children,a=e.as,n=a===void 0?"p":a,l=e.className,d=p(e,["children","as","className"]);return o.createElement(n,i({className:N("card-text",l)},d,{ref:t}),c)});r.propTypes={as:s.elementType,children:s.node,className:s.string};r.displayName="CCardText";export{r as C};
